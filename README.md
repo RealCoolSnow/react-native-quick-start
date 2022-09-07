d821ee136ffcbf272afbd5b4806f91f9cbba3d0f
CoolSnow <coolsnow2020@gmail.com>
2022-09-07 22:14:08 +0800

    feat: ui

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react-native-quick-start
+
+created with (https://github.com/thecodingmachine/react-native-boilerplate)
+
+upgrade to react-native 0.70.1
--- babel.config.js
+++ babel.config.js
@@ -1,51 +1,21 @@
-module.exports = {
-  presets: [
-    [
-      'module:metro-react-native-babel-preset',
-      { useTransformReactJSXExperimental: true },
-    ],
-    '@babel/preset-typescript',
-  ],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: [
-          '.ios.ts',
-          '.android.ts',
-          '.ts',
-          '.ios.tsx',
-          '.android.tsx',
-          '.tsx',
-          '.jsx',
-          '.js',
-          '.json',
-        ],
-        alias: {
-          '@api': './src/api',
-          '@assets': './src/assets',
-          '@common': './src/components/common',
-          '@constants': './src/constants',
-          '@customtypes': './src/types',
-          '@routes': './src/routes',
-          '@screens': './src/components/screens',
-          '@store': './src/redux',
-          '@themes': './src/constants/themes',
-          '@utils': './src/utils',
-        },
+const presets = ['module:metro-react-native-babel-preset']
+const plugins = []
+
+plugins.push(
+  [
+    'module-resolver',
+    {
+      root: ['./src'],
+      extensions: ['.js', '.json'],
+      alias: {
+        '@': './src',
       },
-    ],
-    [
-      '@babel/plugin-transform-react-jsx',
-      {
-        runtime: 'automatic',
-      },
-    ],
-  ],
-  env: {
-    production: {
-      plugins: ['transform-remove-console'],
     },
-  },
+  ],
+  'react-native-reanimated/plugin',
+)
+
+module.exports = {
+  presets,
+  plugins,
 }
